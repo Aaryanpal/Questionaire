babf9dff6ca7bb13fe92f3b71452aa8ed07ffc8d
Aaryan <53212802+Aaryanpal@users.noreply.github.com>
2021-08-18 21:31:38 +0000

    done:code

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
-var readLine = require("readline-sync");
+  //Quiz -- How well do you know me
+  var readlinesync = require("readline-sync");
+  
 
-var userScore = 0;
+  console.log("Let's See how well do you know Aaryan 😉");
 
-var userName = readLine.question("Tell me your name? ");
-console.log('Hiii ' + userName );
+  var userName = readlinesync.question("Namaste \nGive me your name? ") ;
 
-var questionOne = readLine.question("Guess Am i older than 20? ");
-var answerOne = 'yes';
-var userAnswer=readLine.question(questionOne);
+  console.log("\nWelcome, " + userName + ", so DO YOU KNOW Aryan? ");
 
-console.log('You entered '+ userAnswer);
+  console.log("\nNOTE: For each correct answer you will get 1 points. \n");
 
-if(answerOne==='yes')
-{
-  console.log('you are right');
-  userScore=userScore+1;
-  console.log("This is "+userScore)
-}
+  var userScore = 0;
 
-else{
-console.log('you are wrong');
-console.log("Your score "+ userScore);
-}
+  
+
+  //array of objects
+
+  var question = [{
+    question:"\n What's my Full Name? \n",
+    mcq:['Aryan','Aaryan yadav','Aryan Pal'],
+    answer:"2"
+  } ,{
+    question:'what my college name?\n',
+    mcq:['LPU','IPEC','IET'],
+    answer:'1'
+  },{
+    question:'which phone i am using?\n',
+    mcq:['lava','nokia','lenovo'],
+    answer:'2',
+  },{
+    question:'who is my best friend?\n',
+    mcq:['Ram','Gopal','No-one'],
+    answer:'2'
+  },{
+    question:'where do i live?\n',
+    mcq:['Delhi','Agra','Firozabad','None'],
+    answer:'3'
+  }
+  ];
+
+
+  for(var i=0; i < question.length;i++)
+  {
+    console.log("Q" + (i+1));
+    play(question[i]);
+    ;
+  }
+
+
+
+  function play({question1,mcq,answer1})
+  {
+    var userAnswer = readlinesync.keyInSelect(mcq,question1);
+    
+
+    if(userAnswer == answer1)
+    {
+      console.log('\n Yayy!! You are right!');
+    userScore++;
+    
+    }
+    else{
+      console.log('\n OOPs you are wrong');
+      
+      
+    }
+
+  }
+
+  console.log("Your Total is: " +userScore);
+  console.log("-------------------------");
+
+  console.log("Thanks for Playing, Have a nice day \n");
+  console.log("---------------------------------");
