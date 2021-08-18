92e08438a1414798f54ecdb743f718f3afac0968
Aaryan <53212802+Aaryanpal@users.noreply.github.com>
2021-08-18 17:59:37 +0000

    Done with basics

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("I am doing")
+var readLine = require("readline-sync");
+
+var userScore = 0;
+
+var userName = readLine.question("Tell me your name? ");
+console.log('Hiii ' + userName );
+
+var questionOne = readLine.question("Guess Am i older than 20? ");
+var answerOne = 'yes';
+var userAnswer=readLine.question(questionOne);
+
+console.log('You entered '+ userAnswer);
+
+if(answerOne==='yes')
+{
+  console.log('you are right');
+  userScore=userScore+1;
+  console.log("This is "+userScore)
+}
+
+else{
+console.log('you are wrong');
+console.log("Your score "+ userScore);
+}
